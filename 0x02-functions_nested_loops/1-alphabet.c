ec042cb492fa0b4595ff48be1891dae376062f7c
opeyemi <yemilaw547@gmail.com>
2022-12-13 13:22:00 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,35 +4,18 @@
  *
  * Return: Always 0.
  */
-int main(void)
+
+void print_alphabet(void)
 {
-	_putchar('a');
-	_putchar('b');
-	_putchar('c');
-	_putchar('d');
-	_putchar('e');
-	_putchar('f');
-	_putchar('g');
-	_putchar('h');
-	_putchar('i');
-	_putchar('j');
-	_putchar('k');
-	_putchar('l');
-	_putchar('m');
-	_putchar('n');
-	_putchar('o');
-	_putchar('p');
-	_putchar('q');
-	_putchar('r');
-	_putchar('s');
-	_putchar('t');
-	_putchar('u');
-	_putchar('v');
-	_putchar('w');
-	_putchar('x');
-	_putchar('y');
-	_putchar('z');
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
 	_putchar('\n');
 
-	return (0);
+	
 }
