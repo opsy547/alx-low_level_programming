a434927b6697ed08f462ed22eafe9edc74899dd8
opeyemi <yemilaw547@gmail.com>
2022-12-13 13:13:13 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * main - Prints "Alphabets in small letters" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('a');
+	_putchar('b');
+	_putchar('c');
+	_putchar('d');
+	_putchar('e');
+	_putchar('f');
+	_putchar('g');
+	_putchar('h');
+	_putchar('i');
+	_putchar('j');
+	_putchar('k');
+	_putchar('l');
+	_putchar('m');
+	_putchar('n');
+	_putchar('o');
+	_putchar('p');
+	_putchar('q');
+	_putchar('r');
+	_putchar('s');
+	_putchar('t');
+	_putchar('u');
+	_putchar('v');
+	_putchar('w');
+	_putchar('x');
+	_putchar('y');
+	_putchar('z');
+	_putchar('\n');
+
+	return (0);
+}
