94b2715187672a037e516aef2dca7cd7bb935844
opeyemi <yemilaw547@gmail.com>
2022-12-19 15:50:15 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,10 @@
-/*
- * File: 2-strlen.c
- * Auth: Opeyemi Odedoyin
- */
-
-#include "main.h"
-
 /**
  * _strlen - Returns the length of a string.
  * @str: The string to get the length of.
  *
  * Return: The length of @str.
  */
-iny_strlen(char *s)
+int _strlen(char *s)
 {
 	int i = 0;
 
