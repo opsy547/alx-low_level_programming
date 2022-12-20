847b55c6303c1e25c7951714ffc06175883fcc0a
opeyemi <yemilaw547@gmail.com>
2022-12-19 20:20:55 -0800

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,6 @@
  * @str: The string to get the length of.
  * Return: The length of @str.
  */
-
 int _strlen(char *s)
 {
 	int i = 0;
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -22,9 +22,8 @@ void print_array(int *a, int n)
 
 		if (index == n - 1)
 			continue;
-
+		
 		printf(", ");
 	}
-
 	printf("\n");
 }
