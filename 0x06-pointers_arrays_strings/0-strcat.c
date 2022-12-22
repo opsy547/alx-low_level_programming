5ad94831f9d7347e2d68f9161cacbf40d1d252e2
opeyemi <yemilaw547@gmail.com>
2022-12-22 13:15:46 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,25 +6,22 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates two string.
+ * strcat - Concatenates the string pointed to by @src, including the terminating
+ *          null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
- * @src: The pointer source.
+ * @src: The source string to be appended to @dest.
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcat(char *dest, const char *src)
+char *strcat(char *dest, const char *src)
 {
-	int a = -1, i;
+	int index = 0, dest_len = 0;
 
-	for (i = 0; dest[i] != '\0';)
-		i++;
+	while (dest[index++])
+		dest_len++;
 
-	do {
-		a++;
-		dest[i] = src[a];
-		i++;
-
-	} while (src[a] != '\0');
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,5 @@
 /*
- * File: 102-infinite_add.c
+ * File: 103-infinite_add.c
  * Auth: Opeyemi Odedoyin
  */
 
