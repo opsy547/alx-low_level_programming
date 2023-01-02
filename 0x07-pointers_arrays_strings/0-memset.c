08a7425daeebddbbdac723353c94795ec5e3dff2
opeyemi <yemilaw547@gmail.com>
2023-01-02 15:59:10 -0800

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,38 +13,14 @@
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+void *_memset(void *s, int c, size_t n)
 {
-    char buffer[98] = {0x00};
+	unsigned int index;
+	unsigned char *memory = s, value = c;
 
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);
-    return (0);
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
 }
