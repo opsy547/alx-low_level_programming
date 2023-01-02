b0426523534a8616f7d2be04cd769d9fc758ae6c
opeyemi <yemilaw547@gmail.com>
2023-01-02 15:45:27 -0800

    Task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,4 +7,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
+char *_memset(char *s, char b, unsigned int n);
