65a93e6eb471f9a25da35a7cd93554cd41932df3
opeyemi <yemilaw547@gmail.com>
2023-01-02 20:22:24 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,16 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+        unsigned int i, j;
 
-	for (j = 0; *(s + j); j++)
-	{
-		for (i = 0; *(accept + i); i++)
-		{
-			if (*(s + j) == *(accept + i))
-				break;
-		}
-	if (*(accept + i) == '\0')
-		break;
-	}
-	return (j);
-}
+        for (j = 0; *(s + j); j++)
+        {
+                for (i = 0; *(accept + i); i++)
+                {
+                        if (*(s + j) == *(accept + i))
+                                break;
+                }
+        if (*(accept + i) == '\0')
+                break;
+        }
+        return (j)
