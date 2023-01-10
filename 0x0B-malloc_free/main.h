c042416f6f1cc7208e58932162f15b5bb81cab79
opeyemi <yemilaw547@gmail.com>
2023-01-10 15:31:22 -0800

    task 0-6

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,12 +2,13 @@
 #define HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
-void free_grid(int **grid, int height);
-char *str_concat(char *s1, char *s2);
-char *_strdup(char *str);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
-char *argstostr(int ac, char **av);  
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 char **strtow(char *str);
+
 #endif
