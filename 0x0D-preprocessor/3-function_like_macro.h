eae825d57bcb69413143a0383bea7ae3b7f3ae99
opeyemi <yemilaw547@gmail.com>
2023-01-16 14:35:08 -0800

    Task 5

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef ABSO
+#define ABSO
 
-#define ABS(x) ((x < 0) ? (-x) : (x))
+#define ABS(x) (((x) < (0)) ? (-(x)) : (x))
 
-#endif
+#endif /* ABS(X)  */
